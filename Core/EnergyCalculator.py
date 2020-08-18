refactored energy calculation to be coordination number independent + bug fixes

Core/EnergyCalculator.py
@@ -133,40 +133,7 @@ class BayesianRRCalculator(EnergyCalculator):
 
 
 def compute_coefficients_for_linear_topological_model(global_topological_coefficients, symbols, n_atoms):
-    coordination_numbers = [6, 7, 9, 12]
-    symbols_copy = copy.deepcopy(symbols)
-    symbols_copy.sort()
-    symbol_a = symbols_copy[0]
-
-    E_aa_bond = global_topological_coefficients[0]/n_atoms
-    E_bb_bond = global_topological_coefficients[1]/n_atoms
-    E_ab_bond = global_topological_coefficients[2]/n_atoms
-    coordination_energies = {6: global_topological_coefficients[4], 7: global_topological_coefficients[5], 9: global_topological_coefficients[6], 12: 0}
-
-    coefficients = []
-    for symbol in symbols_copy:
-        for cn_number in coordination_numbers:
-            for n_symbol_a_atoms in range(cn_number + 1):
-                E = 0
-                if symbol == symbol_a:
-                    E += 0.1 * global_topological_coefficients[3]
-                    E += (n_symbol_a_atoms*E_aa_bond/2)
-                    E += ((cn_number - n_symbol_a_atoms)*E_ab_bond/2)
-                    E += (coordination_energies[cn_number])
-                else:
-
-                    E += (n_symbol_a_atoms*E_ab_bond/2)
-                    E += ((cn_number - n_symbol_a_atoms)*E_bb_bond/2)
-
-                coefficients.append(E)
-
-    coefficients = np.array(coefficients)
-
-    return coefficients
-
-
-def compute_coefficients_for_linear_topological_model2(global_topological_coefficients, symbols, n_atoms):
-    coordination_numbers = [6, 7, 8, 9, 12]
+    coordination_numbers = list(range(13))
     symbols_copy = copy.deepcopy(symbols)
     symbols_copy.sort()
     symbol_a = symbols_copy[0]
@@ -175,7 +142,6 @@ def compute_coefficients_for_linear_topological_model2(global_topological_coeffi
     E_aa_bond = global_topological_coefficients[0]/n_atoms
     E_bb_bond = global_topological_coefficients[1]/n_atoms
     E_ab_bond = global_topological_coefficients[2]/n_atoms
-    coordination_energies = {6: global_topological_coefficients[4], 7: global_topological_coefficients[5], 8: global_topological_coefficients[6], 9:global_topological_coefficients[7], 12: 0}
 
     coefficients = []
     total_energies = []
@@ -188,7 +154,7 @@ def compute_coefficients_for_linear_topological_model2(global_topological_coeffi
                     E += (global_topological_coefficients[3]*0.1) # careful...
                     E += (n_symbol_a_atoms*E_aa_bond/2)
                     E += ((cn_number - n_symbol_a_atoms)*E_ab_bond/2)
-                    E += (coordination_energies[cn_number])
+                    E += (global_topological_coefficients[4 + cn_number])
 
                     E_tot = E
                     E_tot += n_symbol_a_atoms*E_aa_bond/2
@@ -208,7 +174,8 @@ def compute_coefficients_for_linear_topological_model2(global_topological_coeffi
 
     return coefficients, total_energies
 
-def compute_coefficients_for_linear_topological_model3(global_topological_coefficients, symbols, n_atoms):
+
+def compute_coefficients_for_shape_optimization(global_topological_coefficients, symbols, n_atoms):
     coordination_numbers = list(range(13))
     symbols_copy = copy.deepcopy(symbols)
     symbols_copy.sort()
@@ -221,14 +188,20 @@ def compute_coefficients_for_linear_topological_model3(global_topological_coeffi
         coordination_energies_a[cn] = global_topological_coefficients[index + 1]
 
     coefficients = []
+    total_energies = []
     for symbol in symbols_copy:
         for n_symbol_a_atoms in coordination_numbers:
             E = 0
+            E_tot = 0
             if symbol == symbol_a:
                 E += (n_symbol_a_atoms*E_aa_bond/2)
                 E += (coordination_energies_a[n_symbol_a_atoms])
+
+                E_tot += n_symbol_a_atoms*E_aa_bond/2
                 coefficients.append(E)
+                total_energies.append(E_tot)
     coefficients += [0]*len(coordination_numbers)
+    total_energies += [0]*len(coordination_numbers)
 
     coefficients = np.array(coefficients)
 

Core/GlobalFeatureClassifier.py
@@ -67,32 +67,11 @@ class TopologicalFeatureClassifier(SimpleFeatureClassifier):
     def compute_feature_vector(self, particle):
         n_atoms = particle.get_n_atoms()
         n_aa_bonds, n_bb_bonds, n_ab_bonds = self.compute_respective_bond_counts(particle)
-        n_corners_a = len(particle.get_atom_indices_from_coordination_number([6], symbol=self.symbol_a))
-        n_edge_a = len(particle.get_atom_indices_from_coordination_number([7], symbol=self.symbol_a))
-        n_terrace_a = len(particle.get_atom_indices_from_coordination_number([9], symbol=self.symbol_a))
-
-        M = particle.get_stoichiometry()[self.symbol_a] * 0.1
-
-        feature_vector = np.array([n_aa_bonds/n_atoms, n_bb_bonds/n_atoms, n_ab_bonds/n_atoms, M, n_corners_a, n_edge_a, n_terrace_a])
-        particle.set_feature_vector(self.feature_key, feature_vector)
-
-
-class TopologicalFeatureClassifier2(SimpleFeatureClassifier):
-    def __init__(self, symbols):
-        SimpleFeatureClassifier.__init__(self, symbols)
-        self.feature_key = 'TFC'
-
-    def compute_feature_vector(self, particle):
-        n_atoms = particle.get_n_atoms()
-        n_aa_bonds, n_bb_bonds, n_ab_bonds = self.compute_respective_bond_counts(particle)
-        n_corners_a = len(particle.get_atom_indices_from_coordination_number([6], symbol=self.symbol_a))
-        n_edge_a = len(particle.get_atom_indices_from_coordination_number([7], symbol=self.symbol_a))
-        n_100_terrace_a = len(particle.get_atom_indices_from_coordination_number([8], symbol=self.symbol_a))
-        n_111_terrace_a = len(particle.get_atom_indices_from_coordination_number([9], symbol=self.symbol_a))
+        coordinated_atoms = [len(particle.get_atom_indices_from_coordination_number([cn], symbol=self.symbol_a)) for cn in range(13)]
 
         M = particle.get_stoichiometry()[self.symbol_a]*0.1
 
-        feature_vector = np.array([n_aa_bonds/n_atoms, n_bb_bonds/n_atoms, n_ab_bonds/n_atoms, M, n_corners_a, n_edge_a, n_100_terrace_a, n_111_terrace_a])
+        feature_vector = np.array([n_aa_bonds/n_atoms, n_bb_bonds/n_atoms, n_ab_bonds/n_atoms, M] + coordinated_atoms)
         particle.set_feature_vector(self.feature_key, feature_vector)
 
 
@@ -102,12 +81,13 @@ class TopologicalFeatureClassifier3(SimpleFeatureClassifier):
         self.feature_key = 'TFC'
 
     def compute_feature_vector(self, particle):
-        n_atoms = particle.get_n_atoms()
         n_aa_bonds, n_bb_bonds, n_ab_bonds = self.compute_respective_bond_counts(particle)
-        n_corners_a = len(particle.get_atom_indices_from_coordination_number([6], symbol=self.symbol_a))
-        n_edge_a = len(particle.get_atom_indices_from_coordination_number([7], symbol=self.symbol_a))
-        n_100_terrace_a = len(particle.get_atom_indices_from_coordination_number([8], symbol=self.symbol_a))
-        n_111_terrace_a = len(particle.get_atom_indices_from_coordination_number([9], symbol=self.symbol_a))
+        symbol_a_features = [0]*13
+        for index in particle.get_indices():
+            if particle.get_symbol(index) != 'X':
+                env = particle.get_local_environment(index)
+                symbol_a_features[env[0]] += 1
 
-        feature_vector = np.array([n_aa_bonds/n_atoms, n_bb_bonds/n_atoms, n_ab_bonds/n_atoms, n_corners_a, n_edge_a, n_100_terrace_a, n_111_terrace_a])
+        features = [n_aa_bonds] + symbol_a_features
+        feature_vector = np.array(features)
         particle.set_feature_vector(self.feature_key, feature_vector)

Core/LocalEnvironmentFeatureClassifier.py
@@ -77,34 +77,6 @@ class KMeansClassifier(LocalEnvironmentFeatureClassifier):
         print("Starting kMeans")
         self.kMeans = KMeans(n_clusters=self.n_cluster, random_state=0).fit(local_environments)
 
-
-class TopologicalEnvironmentClassifier(LocalEnvironmentFeatureClassifier):
-    def __init__(self, local_environment_calculator):
-        LocalEnvironmentFeatureClassifier.__init__(self, local_environment_calculator)
-        self.feature_key = 'TEC'
-
-    def compute_n_features(self, particle):
-        return 76
-
-    def predict_atom_feature(self, particle, lattice_index, recompute_local_environment=False):
-        symbol = particle.get_symbol(lattice_index)
-        symbols = sorted(particle.get_contributing_symbols())
-        symbol_index = symbols.index(symbol)
-
-        element_offset = symbol_index*38
-
-        if recompute_local_environment:
-            self.local_environment_calculator.compute_local_environment(particle, lattice_index)
-
-        environment = particle.get_local_environment(lattice_index)
-        coordination_number = np.sum(environment)
-        coordination_number_offsets = {6: 0, 7: 7, 9: 15, 12: 25}
-
-        atom_feature = element_offset + coordination_number_offsets[coordination_number] + environment[0]
-
-        return atom_feature
-
-
 class TopologicalEnvironmentClassifier2(LocalEnvironmentFeatureClassifier):
     def __init__(self, local_environment_calculator, symbols):
         LocalEnvironmentFeatureClassifier.__init__(self, local_environment_calculator)
@@ -115,22 +87,22 @@ class TopologicalEnvironmentClassifier2(LocalEnvironmentFeatureClassifier):
         self.feature_key = 'TEC'
 
     def compute_n_features(self, particle):
-        return 94
+        return 182
 
     def predict_atom_feature(self, particle, lattice_index, recompute_local_environment=False):
         symbol = particle.get_symbol(lattice_index)
         symbol_index = self.symbols.index(symbol)
 
-        element_offset = symbol_index*47
+        element_offset = symbol_index*91
 
         if recompute_local_environment:
             self.local_environment_calculator.compute_local_environment(particle, lattice_index)
 
         environment = particle.get_local_environment(lattice_index)
         coordination_number = len(particle.neighbor_list[lattice_index])
-        coordination_number_offsets = {6: 0, 7: 7, 8: 15, 9: 24, 12: 34}
+        coordination_number_offsets = [int(cn*(cn + 1)/2) for cn in range(13)]
 
-        atom_feature = element_offset + coordination_number_offsets[coordination_number] + environment[0]
+        atom_feature = int(element_offset + coordination_number_offsets[coordination_number] + environment[0])
 
         return atom_feature
 
@@ -168,5 +140,3 @@ class TopologicalEnvironmentClassifier3(LocalEnvironmentFeatureClassifier):
         atom_feature = element_offset + self.coordination_number_offsets[coordination_number]
 
         return atom_feature
-
-